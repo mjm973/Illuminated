73b5aa8a41cc772d1f82b92361879aade5f402ff
Mateo Juvera <mjm973@nyu.edu>
2018-03-25 16:36:47 +0400

    basic avatar movement and camera using mouse + WASD

--- Assets/Scripts/Managers/InputManager.cs
+++ Assets/Scripts/Managers/InputManager.cs
@@ -7,16 +7,63 @@ using Photon;
 // Class to manage player input
 public class InputManager : Photon.MonoBehaviour {
 
+    public float moveSpeed = 5;
+    public float angularSpeed = 120;
+    public bool invertY = true;
+
+    Transform head;
+
 	// Use this for initialization
 	void Start () {
-		
+        // Lock cursor to center of the screen to make our lives easier
+        Cursor.lockState = CursorLockMode.Locked;
+        // locate our head
+        head = transform.Find("Head");
+        // for now, let's grab the main camera and attach it
+        Transform mainCam = Camera.main.transform;
+        mainCam.position = head.position;
+        mainCam.rotation = head.rotation;
+        mainCam.SetParent(head);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         // allow input only if we own the gameobject
 		if (photonView.isMine) {
-            
+            HandleInput();
         }
 	}
+
+    void HandleInput() {
+        Move();
+        Turn();
+    }
+
+    void Move() {
+        // make vector to store movement input
+        Vector3 move = new Vector3();
+        // capture WASD/ArrowKey input
+        move.x = Input.GetAxis("Horizontal");
+        move.z = Input.GetAxis("Vertical");
+        // normalize and multiply by our speed
+        move = move.normalized * moveSpeed;
+        // translate scaled by deltaTime
+        transform.Translate(move * Time.deltaTime);
+    }
+
+    void Turn() {
+        // make vector to store rotation input
+        Vector3 turn = new Vector3();
+        // horizontal mouse -> rotate around y-axis
+        turn.y = Input.GetAxis("Mouse X");
+        // vertical mouse -> rotate around x-axis
+        turn.x = Input.GetAxis("Mouse Y") * (invertY ? -1 : 1);
+        // scale by our angular speed
+        turn *= angularSpeed * Time.deltaTime;
+
+        // horizontal turn is in world space
+        transform.Rotate(0, turn.y, 0, Space.World);
+        // vertical turn is in local space
+        head.Rotate(turn.x, 0, 0, Space.Self);
+    }
 }
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -6,6 +6,12 @@ using Photon;
 // Class to sync player to network
 public class PlayerManager : Photon.MonoBehaviour {
 
+    Vector3 targetPos;
+    Quaternion targetRot;
+
+    [Range(1, 10)]
+    public float interpolationFactor = 5f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -15,15 +21,19 @@ public class PlayerManager : Photon.MonoBehaviour {
 	void Update () {
         // update gameobject of we don't own it
 		if (!photonView.isMine) {
-
+            float factor = Time.deltaTime * interpolationFactor;
+            transform.position = Vector3.Lerp(transform.position, targetPos, factor);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, factor);
         }
 	}
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
         if (stream.isWriting) { // write to network
-
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
         } else { // read from network
-
+            targetPos = (Vector3)stream.ReceiveNext();
+            targetRot = (Quaternion)stream.ReceiveNext();
         }
     }
 }
