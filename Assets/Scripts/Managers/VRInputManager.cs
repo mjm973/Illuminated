ae6dfb989393b613dad47014f112160714f43755
RocketPlumber <sb5455@nyu.edu>
2018-03-29 05:24:58 -0800

    VR shooting and teleport now functional!

---
+++ Assets/Scripts/Managers/VRInputManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon;
+using VRTK;
+
+public class VRInputManager : Photon.MonoBehaviour {
+
+    [SerializeField]
+    VRTK_ControllerEvents left;
+    [SerializeField]
+    VRTK_ControllerEvents right;
+
+    GrenadeGunManager gun;
+
+    // Use this for initialization
+    void Start () {
+        gun = GetComponentInChildren<GrenadeGunManager>();
+        right.TriggerClicked += new ControllerInteractionEventHandler(OnTriggerClicked);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerClicked(object sender, ControllerInteractionEventArgs e) {
+        gun.Fire();
+    }
+}
