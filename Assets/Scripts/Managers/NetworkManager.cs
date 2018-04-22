7017a7a731fe5f1becd027757b9c8baa0cc36a02
Mateo Juvera <mjm973@nyu.edu>
2018-04-22 23:26:55 +0400

    whoops now GM actually instantiates

--- Assets/Scripts/Managers/NetworkManager.cs
+++ Assets/Scripts/Managers/NetworkManager.cs
@@ -50,6 +50,7 @@ public class NetworkManager : Photon.PunBehaviour {
         else if (PhotonNetwork.room == null) {
             if (GUI.Button(new Rect(100, 100, 250, 100), "New " + roomNames[randIndex])) {
                 PhotonNetwork.CreateRoom(roomNames[randIndex], new RoomOptions() { MaxPlayers = 6, IsVisible = true }, null);
+                PhotonNetwork.Instantiate("GM", Vector3.zero, Quaternion.identity, 0);
             }
 
             if (rooms != null) {
